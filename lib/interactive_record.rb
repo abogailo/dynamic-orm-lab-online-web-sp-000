6b4b1e674e29f724dcd14c53609b89d96503b6d0
Abagail Hudson <abagailanne@gmail.com>
2019-07-13 17:41:58 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,4 +1,4 @@
-require 'active_support/inflector'
+
 class InteractiveRecord
 
      def initialize(options={})
@@ -55,3 +55,4 @@ class InteractiveRecord
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
     DB[:conn].execute(sql)
   end
+end
