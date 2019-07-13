bd524cd8ccfdb25a7f11f6df102405a6a521e67b
Abagail Hudson <abagailanne@gmail.com>
2019-07-13 17:30:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,12 +4,12 @@ require 'active_support/inflector'
 class InteractiveRecord
 
 
-  def self.table_name 
+  def self.table_name
     #creats a downcased plural table name based on Class name
-    self.to_s.downcase.pluralize 
+    self.to_s.downcase.pluralize
   end
 
-   def self.column_names 
+   def self.column_names
     # returns an array of SQL column names
     #hash of columns names
     #return them as a an array of strings
@@ -19,7 +19,7 @@ class InteractiveRecord
      #give you the hash of info
     sql = "PRAGMA table_info('#{table_name}')"
 
-     table_info = DB[:conn].execute(sql) 
+     table_info = DB[:conn].execute(sql)
 
      # creates attr_accessors for each column name
     column_names = []
@@ -31,11 +31,11 @@ class InteractiveRecord
   end
 
    #creates new student with attributes
-  def initialize(objects = {}) #pass in a hash 
+  def initialize(objects = {}) #pass in a hash
     objects.each do |key,value|
       self.send("#{key}=",value)
     end
-  end  
+  end
 
    # save - INSERT INTO students (name, grade) VALUES (x,y)
   def save
@@ -44,18 +44,18 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-   def table_name_for_insert 
+   def table_name_for_insert
     # returns the table name when called on an instance of Student
     self.class.table_name
   end
 
-   def col_names_for_insert 
-    # return column names when called on an instance of Student does not include id column 
+   def col_names_for_insert
+    # return column names when called on an instance of Student does not include id column
     #returns it as a string ready to be inserted into a sql statement
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end 
+  end
 
-   def values_for_insert 
+   def values_for_insert
     #formats the column names to be used in SQL statement
     #use the column_names array, iterate over it to get the attribute names
     #and then user the attribute = method with send to assign the value
@@ -63,8 +63,8 @@ class InteractiveRecord
     self.class.column_names.each do |col_name|
       #get the value for each attribute name
       values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end  
-    values.join(", ")  
+    end
+    values.join(", ")
   end
 
    def self.find_by_name(name)
@@ -87,4 +87,4 @@ class InteractiveRecord
 
      DB[:conn].execute(sql, value_name);
   end
-end 	
+end
